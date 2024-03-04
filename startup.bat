ad03f424da3e3086856f45baad335e1d8de18d27
Giovanni De Giorgio <giovannidegiorgio1999@gmail.com>
2024-03-04 20:58:52 +0100

    feat: add docker commands in startup scripts

--- startup.bat
+++ startup.bat
@@ -1,3 +1,11 @@
 @echo off
+set SONS_OF_THE_DOCKER_IMAGE_NAME=sons-of-the-docker
+set SONS_OF_THE_DOCKER_CONTAINER_NAME=sons-of-the-docker
+
+
+
 echo "Starting up your SOTF Server..."
 echo "Building your docker image..."
+CMD /c docker build . -t %SONS_OF_THE_DOCKER_IMAGE_NAME%
+CMD /c docker tag %SONS_OF_THE_DOCKER_IMAGE_NAME% %SONS_OF_THE_DOCKER_IMAGE_NAME%:latest
+CMD /c docker run -d --name %SONS_OF_THE_DOCKER_CONTAINER_NAME% %SONS_OF_THE_DOCKER_IMAGE_NAME%
--- startup.sh
+++ startup.sh
@@ -1,3 +1,8 @@
 !#bin/sh
+export SONS_OF_THE_DOCKER_IMAGE_NAME=sons-of-the-docker
+export SONS_OF_THE_DOCKER_CONTAINER_NAME=sons-of-the-docker
 echo "Starting up your SOTF Server..."
 echo "Building your docker image..."
+docker build . -t $SONS_OF_THE_DOCKER_IMAGE_NAME
+docker tag SONS_OF_THE_DOCKER_IMAGE_NAME SONS_OF_THE_DOCKER_IMAGE_NAME:latest
+docker run -d --name $SONS_OF_THE_DOCKER_CONTAINER_NAME SONS_OF_THE_DOCKER_IMAGE_NAME
