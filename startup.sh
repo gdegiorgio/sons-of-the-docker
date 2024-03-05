b80fd4dab54c18060d9982574dcba216141b101a
Giovanni De Giorgio <giovannidegiorgio1999@gmail.com>
2024-03-05 22:25:37 +0100

    chore: edit startup.sh

--- startup.sh
+++ startup.sh
@@ -8,4 +8,4 @@ echo "Starting up your SOTF Server..."
 echo "Building your docker image..."
 docker build . -t $SONS_OF_THE_DOCKER_IMAGE_NAME
 docker tag SONS_OF_THE_DOCKER_IMAGE_NAME SONS_OF_THE_DOCKER_IMAGE_NAME:latest
-docker run -d --name $SONS_OF_THE_DOCKER_CONTAINER_NAME SONS_OF_THE_DOCKER_IMAGE_NAME
+docker run -d --name -p 8766:8766/udp -p 27016:27016/udp -p 9700:9700/udp $SONS_OF_THE_DOCKER_CONTAINER_NAME SONS_OF_THE_DOCKER_IMAGE_NAME +quit
